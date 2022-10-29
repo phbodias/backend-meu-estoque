2a311db8284c57bdf9e15bf464039ba54e6e796e
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-29 02:23:32 -0300

    :wrench: chore: Configurando app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT: number = Number(process.env.PORT) || 5009;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
