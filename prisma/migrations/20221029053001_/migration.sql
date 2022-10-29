9ab7e16cb2d236fb274816f9cd6223612cce7079
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-29 18:51:14 -0300

    :sparkles: Conexão com o prisma

---
+++ prisma/migrations/20221029053001_/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ONLY_READ', 'SELLER', 'MANAGER', 'OWNER');
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'ONLY_READ',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
